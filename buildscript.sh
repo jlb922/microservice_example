64860fb08d6f97359d9a4996eec5ec1eab953c3d
Jeff Bradbury <jlb922@gmail.com>
2020-04-26 12:39:29 -0400

    consolidated into readme.md

--- buildscript.sh
+++
@@ -1,1 +0,0 @@
-docker build --build-arg SOURCE_LOCATION=/Users/jbrad/go/src/github.com/jlb922/microservice_example --no-cache -t jlb922/go-mongo-microservice:latest .
--- runscript.sh
+++
@@ -1,2 +0,0 @@
-#docker run --name go-mongo-microservice -d --rm -p 8000:9090 --network dockercompose_network1 jlb922/go-mongo-microservice:latest
-docker run --name go-mongo-microservice -d --rm -p 8000:9090 jlb922/go-mongo-microservice:latest /bin/sh
